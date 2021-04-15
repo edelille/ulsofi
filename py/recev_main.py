b01b7085d752e6d365bfd86734387697b553635c
Lam Nguyen <lamn@cec.sc.edu>
2021-04-15 13:53:17 -0700

    added working fft

--- py/recev_main.py
+++ py/recev_main.py
@@ -1,27 +1,89 @@
 import scipy.fft as sfft
 import numpy as np  # for arrays
-# import pyaudio
-# import analyse
+import pyaudio
+import time
+import matplotlib
+matplotlib.use('GTK3Agg')
+from matplotlib import pyplot as plt
+
+blit = True
+
 
 # Main runtime
 if __name__ == '__main__':
     print('Starting UlSoFi-Py')
 
-    x = np.array(np.arange(10))
-    # using scipy fft
-    res = sfft.rfft(x)
-
-    for x in res:
-        print(x)
-
     pyaud = pyaudio.PyAudio()
-    stream = pyaud.open( format = pyaudio.paInt16, channels = 1, rate = 44100, input_device_index = 2, input = True)
+    stream = pyaud.open( format = pyaudio.paInt16, channels = 1, rate = 44100, input_device_index = 2, input = True, frames_per_buffer=4096)
     
+
+    plt.ion()
+    X = np.linspace(0,22050,205)
+    y = np.cos(X)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2,1,1)
+    ax2 = fig.add_subplot(2,1,2)
+
+    img = ax1.imshow(X, vmin=-1, vmax=1, interpolation="None", cmap="RdBu")
+
+    line1 = ax2.plot([], lw=3)
+
+    ax1.set_xlim(0, 22000)
+    ax2.set_ylim([0,1000])
+
+    plt.title("Dynamic Plot of mic input",fontsize=25)
+    plt.xlabel("Frequency (Hz)",fontsize=18)
+    plt.ylabel("Intensity",fontsize=18)
+
+    fig.canvas.draw()
+
+    # If blit, set up some blits
+    if blit:
+        axbackground = fig.canvas.copy_from_bbox(ax1.bbox)
+        ax2background = fig.canvas.copy_from_bbox(ax2.bbox)
+
+    plt.show(block=False)
+
     while True: 
         # Read raw microphone data 
-        rawsamps = stream.read(1024) 
+        rawsamps = stream.read(4096, exception_on_overflow = False) 
         # Convert raw data to NumPy array 
-        samps = numpy.fromstring(rawsamps, dtype=numpy.int16) 
-        # Show the volume and pitch 
-        print(analyse.loudness(samps), analyse.musical_detect_pitch(samps))
+        samps = np.fromstring(rawsamps, dtype=np.int16) 
+        # Calculate the FFT
+        pfft = sfft.rfft(samps)
+
+        # Parse the FFT
+        ydata = [0]*int(len(pfft)/10 + 1)
+        for i in range(len(pfft)):
+            ydata[int(i/10)] += pfft[i]
+
+
+        # DEBUG Space
+        #print("Length of all: ", len(rawsamps), len(ydata), len(pfft))
+        
+        
+        line1.set_xdata(X)
+        line1.set_ydata(ydata)
+        
+
+        if blit:
+            fig.canvas.restore_regeion(axbackground)
+            fig.canvas.restore_regeion(ax2background)
+
+            ax1.draw_artist(img)
+            ax2.draw_artist(line)
+            ax2.draw_artist(text)
+
+            # Fill in the axes
+            fig.canvas.blit(ax1.bbox)
+            fig.canvas.blit(ax2.bbox)
+
+        else:
+            fig.canvas.draw()
+
+        fig.canvas.flush_events()
+
+        
+
 
--- py/trans_main.py
+++ py/trans_main.py
@@ -6,15 +6,49 @@ from sound_api import *
 ############## GLOBAL VARIABLES ##############
 sample_rate = 44100 # Hz
 sample_packet = 4096
+ 
+############## ALIASES FOR BITS ##############
+A1 = 440    # 420-460
+A2 = 494    # 
+A3 = 523
+A4 = 587
+A5 = 659
+A6 = 698
+B1 = A1 * 2
+B2 = A2 * 2
+B3 = A3 * 2
+B4 = A4 * 2
+B5 = A5 * 2
+B6 = A6 * 2
+
+
+D1 = [5000]
+
+# I attempt to send bits:   1111 1111, 00
+# I actually receive:       1111 1111, 01
+
+# Args: Insert a byte, play a chord, which is a function of the byte
+def trans_bits8(byte):
+    pass
+
+def trans_packet100(bytearr):
+    pass
+
+def trans_file(filename):
+    pass
 
 
 def init():
     pygame.mixer.pre_init(sample_rate, -16, 1) # 44.1kHz, 16-bit signed, mono
-    pygame.init()
+    pygame.mixer.init()
 
 if __name__ == '__main__':
     # Initialize pygame to play sounds
     init()
-
     
-    play_for(sine_wave(440, sample_packet), 1000)
+    while True:
+        for fv in D1:
+            play_for(sine_wave(fv, sample_packet), 1000)
+        
+
+
