b8d1cc252ba446dfac46c9f36471edd2ce5c561c
Lam Nguyen <lamn@cec.sc.edu>
2021-05-03 18:35:14 -0700

    receiver is able to receive a file

--- py/recev_main.py
+++ py/recev_main.py
@@ -9,10 +9,13 @@ from matplotlib import pyplot as plt
 blit = False
 
 ############## GLOBAL VARIABLES ##############
-sample_rate = 44100 # Hz
-sample_packet = 16394
+sample_rate = 48000 # Hz
+sample_packet = 4096
 FLU = {}    # Frequency Lookup
-FFT_Threshold = 2000       # Guess based on data
+FFT_Threshold = 1000       # Guess based on data
+NREP = 2
+soft_fft_range = 6
+STOP_CODON = '0000000011'
 
 ## GENERATE FREQUENCY ARRAY BASED ON INTERVAL ##
 min     = 400   # Minimum frequency
@@ -22,6 +25,26 @@ pamt    = 5     # Amount of packets
 D = []          # Initializing of D array: int
 G = []          # Initializing of G array: array<int>
 for i in range(pamt): G.append([])
+freqs = sfft.fftfreq(sample_packet, d=(1/sample_rate))
+hexd = {
+    '0000': '0',
+    '0001': '1',
+    '0010': '2',
+    '0011': '3',
+    '0100': '4',
+    '0101': '5',
+    '0110': '6',
+    '0111': '7',
+    '1000': '8',
+    '1001': '9',
+    '1010': 'a',
+    '1011': 'b',
+    '1100': 'c',
+    '1101': 'd',
+    '1110': 'e',
+    '1111': 'f',
+}
+
 
 def setup_G_arr():
     for i in range(pamt * psize):
@@ -112,11 +135,11 @@ def find_closestFFT(arr, target):
         
     return q, res, dist
 
-
-
-def enter_receiver(pyaud, stream):
+def enter_parseFrequencies(pyaud, stream):
+    foundlt = False
     while True:
-        starttime = time.time()
+        fftres = []
+        for i in range(psize): fftres.append('')
 
         # Read raw microphone data 
         rawsamps = stream.read(sample_packet, exception_on_overflow = False) 
@@ -125,34 +148,220 @@ def enter_receiver(pyaud, stream):
         # Calculate the FFT
         pfft = sfft.rfft(samps)
 
+        found = False
+        for i in range(int(len(freqs)/2) - 1):
+            # check each one that exceeds the limit
+            itr = abs(np.real(pfft[i]))
+            if itr > FFT_Threshold:
+                found = True
+                print('freq: {}\t exceeded the threshold at {}'.format(freqs[i], itr))
 
-        # Print the result of many fft results
-        specific_crno = 0
-        fftres = ''
-        for i in range(len(G[specific_crno])): # TODO first crno only
-            pfft_i = FLU[G[specific_crno][i]]
-            sres = pfft[pfft_i]  # fft output at FLU point
-            print("frequency: {}\tFLU:{}\tabs(fftres): {}\tfftres: {}".format(D[i], FLU[D[i]], abs(np.real(sres)), sres))
+        if found == True and foundlt == False:
+            print('parse done')
+    
+        fountlt = found
 
-            if i == len(G[specific_crno]) - 2:
-                fftres += ', '
-            if sres > FFT_Threshold:
-                fftres += '1'
-            else:
-                fftres += '0'
+def readFromCRNO(cmem=None, specific_crno=0):
+    # Read raw microphone data 
+    rawsamps = stream.read(sample_packet, exception_on_overflow = False) 
+    # Convert raw data to NumPy array 
+    samps = np.fromstring(rawsamps, dtype=np.int16) 
+    # Calculate the FFT
+    pfft = sfft.rfft(samps)
+    # Print the result of many fft results
+    freqs_broken = []
+    if cmem is None:
+        cmem = []
+        for i in range(psize): cmem.append('')
 
-        print('crno #{} from {} to {}\t=> {}\t currTime: {}'.format(
-            specific_crno,
-            G[specific_crno][0],
-            G[specific_crno][len(G[specific_crno])-1],
-            fftres,
-            time.time()
-        ))
+    for i in range(len(G[specific_crno])): # TODO first crno only
+        # Check all outputs around the set FLU point
+        thres_broken = False
+        sres_arr = []
+        FLU_centerpoint = FLU[G[specific_crno][i]]
 
-        #time.sleep(1)
+        for j in range(soft_fft_range):
+            # debug this
+            pfft_i = FLU_centerpoint - int(soft_fft_range / 2) + j
+
+            if abs(np.real(pfft[pfft_i])) > FFT_Threshold:  # fft output at FLU point
+                #print(abs(np.real(pfft[pfft_i])), pfft_i, freqs[pfft_i])
+                if str(int(freqs[pfft_i])) not in freqs_broken: freqs_broken.append(str(int(freqs[pfft_i])))
+                thres_broken = True
+
+        #print("frequency: {}\tFLU:{}\tabs(fftres): {}\tfftres: {}".format(D[i], FLU[D[i]], abs(np.real(sres)), sres))
+        # conditional set to counter the the false points
+        if thres_broken:
+            cmem[i] = '1'
+        elif cmem[i] != '1':
+            cmem[i] = '0'
+
+    return cmem
+
+def enter_receiver(pyaud, stream, iter=0):
+    print('waiting for preamble')
+    enter_preambleRec(stream)
+
+    recorded_memory = []
+    lastbyte = ''
+
+    while True:
+        starttime = time.time()
+
+        # Start of procedure
+        fftres = []
+        for i in range(psize): fftres.append('')
+
+        sc_count = 0
+        while True:
+            fftres = readFromCRNO()
+
+            print(fftres)
+            if ''.join(fftres) == STOP_CODON:
+                sc_count += 1
+                if sc_count > 3:
+                    write_output(recorded_memory, no=iter)
+                    return
+
+            if check_parityBits(fftres):
+                break
+        
+        currbyte = ''.join(fftres[:len(fftres)-2])
+        # if this byte is the same as last byte skip it
+        print("curr: {},\tlast: {}".format(currbyte, lastbyte))
+        if currbyte == lastbyte:
+            continue
+
+        recorded_memory.append(currbyte)
+
+        lastbyte = currbyte
 
         elapsed = time.time() - starttime
-        #print("it took {} for one sample".format(elapsed))
+
+        print('{} to {}\t=> {}\t elapsed: {},\t'.format(
+            G[0][0],
+            G[0][len(G[0])-1],
+            ' '.join(fftres),
+            elapsed,
+        ))
+    
+# At this point, output the recorded memory to a file
+def write_output(recorded_memory, no=0):
+    count = 0
+    filename = 'output{}.txt'.format(no)
+    with open(filename, 'w') as f:
+        for i in range(len(recorded_memory)):
+            count += 1
+            tl = translate_bin2hex(recorded_memory[i])
+            f.write(tl+ ' ')
+            if count == 15:
+                f.write('\n')
+                count = 0
+
+
+
+
+
+
+def translate_bin2hex(bin):
+    fnib = bin[:4]
+    lnib = bin[4:]
+    print(fnib, lnib, bin)
+
+    res = hexd[fnib] + hexd[lnib]
+    return res
+
+# Receives an array, in which there are parity bits being a modulo
+def check_parityBits(inp, pbits=2):
+    # determine the checksum
+    chksum = 0
+    for i in range(len(inp) - pbits):
+        if inp[i] == '1':
+            chksum += 1
+
+    # determine the parity modulo remainer
+    modsum = 0
+    for i in range(pbits):
+        if inp[len(inp) - 1 - i] == '1':
+            modsum += 2 ** i
+
+    #print('input {}, has checksum {} and modsum {}'.format( inp, chksum, modsum))
+    if chksum % 2 ** pbits == modsum:
+        return True
+    else:
+        return False
+
+
+    
+
+
+def enter_preambleRec(stream):
+
+    # Preamble code
+    pac = [
+        '10101010',
+        '11110000',
+        '00001111',
+        '01010101'
+    ]
+    pacp = 0 # preamble code position
+
+
+    while pacp < 4:
+        fftres = []
+        for i in range(psize): fftres.append('')
+        freqs = sfft.fftfreq(sample_packet, d=(1/sample_rate))
+
+        while True:
+            fftres = readFromCRNO()
+
+            # check parity bits, if correct no need to repeat
+            if (check_parityBits(fftres)):
+                #print('correct parity: {}'.format(fftres))
+                break
+        
+        if (check_parityBits(fftres)) and ''.join(fftres) != '0000000000':
+            if ''.join(fftres[:len(fftres)-2]) == pac[pacp]:
+                #print('preamble #{} has been recognized: {}'.format(pacp, ''.join(fftres[:len(fftres)-2])))
+                pacp += 1
+
+        
+
+
+
+def test_preambleRec(stream):
+    min_time = 10000
+    max_time = 0
+    mean = 0
+    over2 = 0
+    over5 = 0
+    over10 = 0
+    ntests = 50
+    for i in range(ntests):
+        full_cycle_dur = 1
+        starttime = time.time()
+        enter_preambleRec(stream)
+        elapsed = time.time() - starttime
+
+        # Skip the first one for stats purposes
+        if i == 0: continue
+
+        if elapsed < min_time: min_time = elapsed
+        if elapsed > max_time: max_time = elapsed
+        if elapsed > 2: over2 += 1
+        if elapsed > 5: over5 += 1
+        if elapsed > 10: over10 += 1
+
+        mean += elapsed
+
+        print('correctly recognized preamble #{} in {} seconds'.format(i,elapsed))
+
+    print("mean: {}, min_time: {}, max_time: {}, over2: {}, over5: {}, over10: {}, 100 tests completed".format(
+        mean/ntests, min_time, max_time,over2, over5, over10))
+
+
+
+        
 
 def init():
     # calculate the indices for each fft component
@@ -170,13 +379,18 @@ if __name__ == '__main__':
     print('Starting UlSoFi-Py')
 
     pyaud = pyaudio.PyAudio()
-    stream = pyaud.open( format = pyaudio.paInt16, channels = 1, rate = 44100, input_device_index = 2, input = True, frames_per_buffer=sample_packet)
+    stream = pyaud.open( format = pyaudio.paInt16, channels = 1, rate = sample_rate, input_device_index = 2, input = True, frames_per_buffer=sample_packet)
     
     init()
 
 
     #enter_fftVis(pyaud, stream)
-    enter_receiver(pyaud, stream)
+    for i in range(10):
+        enter_receiver(pyaud, stream, iter=i)
+    #enter_preambleRec(stream)
+    #test_preambleRec(stream)
+
+    #enter_parseFrequencies(pyaud, stream)
     
 
         
